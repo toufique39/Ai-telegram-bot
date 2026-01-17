f502a1cc4a20b105b774d8dc419c2b61ca82d0bd
Toufique Ahmed <tasniaislam112233@gmail.com>
2026-01-17 17:02:18 +0600

    Update ai_engine.py

--- ai_engine.py
+++ ai_engine.py
@@ -1,5 +1,6 @@
 import requests
-GROQ_API_KEY=" "
+import os
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 GROQ_URL = "https://api.groq.com/openai/v1/chat/completions"
 MODEL_NAME = "llama-3.1-8b-instant"
 
